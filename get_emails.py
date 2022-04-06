df2b57d5ebcd1b9782104e317cc71c558592d25f
GPPMM_PC <kostiantyn.vasko@nure.ua>
2022-04-06 09:58:42 +0200

    Initial commit

---
+++ get_emails.py
@@ -0,0 +1,38 @@
+import email
+import imaplib
+from email import policy
+
+
+class ReceiveMails:
+    def __init__(self, conf):
+        self.n = 0
+        self.heads = []
+        self.froms = []
+        self.text = []
+        self.filepath = []
+        self.__conf = conf
+        self.__server = self.__conf['user']['server']
+
+    def __call__(self):
+        mail = imaplib.IMAP4_SSL(self.__server)
+        _,_ = mail.login(self.__conf['user']['email'],
+                         self.__conf['user']['password'])
+        mail.list()
+        mail.select('inbox')
+        self.n = 0
+        self.heads = []
+        self.froms = []
+        (retcode, messages) = mail.search(None, '(UNSEEN)')
+        if retcode == 'OK':
+            for num in messages[0].split():
+                print('Processing ')
+                self.n = self.n + 1
+                typ, data = mail.fetch(num, '(RFC822)')
+                for response_part in data:
+                    if isinstance(response_part, tuple):
+                        original = email.message_from_bytes(response_part[1], policy=policy.default)
+                        self.froms.append(original['From'])
+                        self.heads.append(original['Subject'])
+                        _, _ = mail.store(num, '+FLAGS', '\\Seen')
+
+            return self.n, self.froms, self.heads
---
+++ telegram_main.py
@@ -0,0 +1,82 @@
+import sys
+import threading
+import time
+from get_emails import ReceiveMails
+import telebot  # type: ignore
+import logging
+import yaml
+
+# log level
+logging.basicConfig(level=logging.INFO)
+conf = yaml.safe_load(open("conf/application.yml"))
+chat_id = conf['telegram']['chat_id']
+telbot = telebot.TeleBot(conf['telegram']['TOKEN'])  # init
+ReceiveMails_inst = ReceiveMails(conf)
+link = conf['user']['link']
+
+
+@telbot.message_handler(commands=['start'])
+def welcome(message):
+    if message.chat.id != chat_id:
+        telbot.send_message(message.chat.id, text="Leave this bot it's not for you")
+        return
+    telbot.send_message(message.chat.id, text=f"Ready to check your mails {message.chat.first_name}")
+
+
+@telbot.message_handler(commands=['get'])  # type: ignore
+def welcome(message):
+    if message.chat.id != chat_id:
+        telbot.send_message(message.chat.id, text="Leave this bot it's not for you")
+        return
+    n, froms, heads = ReceiveMails_inst()
+    if n > 0:
+        if n > 1:
+            for letter_num in range(n):
+                tel_send(letter_num, froms, heads)
+        else:
+            tel_send(0, froms, heads)
+    else:
+        telbot.send_message(message.chat.id, text="There is no letters")
+
+
+def telbot_tread():
+    while True:
+        try:
+            telbot.polling(none_stop=True, interval=1)
+        except Exception:
+            print('Telebot error: ', sys.exc_info())
+
+
+def email_tread():
+    global chat_id
+    while True:
+        time.sleep(60)
+        try:
+            n, froms, heads = ReceiveMails_inst()
+            if n > 0:
+                if n > 1:
+                    for letter_num in range(n):
+                        tel_send(letter_num, froms, heads)
+                else:
+                    tel_send(0, froms, heads)
+        except Exception:
+            print('Email error: ', sys.exc_info())
+
+
+def tel_send(letter_num, froms, heads):
+    text = f"From: {froms[letter_num]}\n" \
+           f"Subject: {heads[letter_num]}\n" \
+           f"[login]({link})"
+    telbot.send_message(chat_id, text=text, parse_mode='Markdown')
+
+
+def main():
+    telbot_trd = threading.Thread(target=telbot_tread)
+    email_trd = threading.Thread(target=email_tread)
+
+    telbot_trd.start()
+    email_trd.start()
+
+
+if __name__ == "__main__":
+    main()
